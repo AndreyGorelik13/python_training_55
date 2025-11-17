2d9d8f697f485e911f232bb43ad55e3767879b9f
Горелик  Андрей <Andrey.Gorelik@fors.ru>
2025-11-17 23:58:14 +0300

    Задание №14: Реализовать проверки для всех полей контакта на главной странице - исправлен

--- test/test_contact_view.py
+++ test/test_contact_view.py
@@ -20,4 +20,4 @@ def merge_phones_like_home_page(contact):
     return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter(lambda x: x is not None, [contact.home, contact.mobile, contact.work]))))
 
 def merge_emails_like_home_page(contact):
-    return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
+    return "\n".join(filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))
