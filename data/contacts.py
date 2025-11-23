0f5d6122d3ffa61c28b9ba24567bb5084f83f1b8
Горелик  Андрей <Andrey.Gorelik@fors.ru>
2025-11-23 21:45:55 +0300

    Задание №18: Реализовать генераторы тестовых данных

---
+++ data/contacts.py
@@ -0,0 +1,14 @@
+from model.address import Address
+
+testdata = [
+    Address(firstname="firstname1", middlename="middlename1",
+            lastname="lastname1", nickname="nickname1", title="title1",
+            company="company1", address="address1", home="home1", mobile="mobile1",
+            work="work1", fax="fax1", email="email1", email2="email2_1", email3="email3_1",
+            homepage="homepage1"),
+Address(firstname="firstname2", middlename="middlename2",
+            lastname="lastname2", nickname="nickname2", title="title2",
+            company="company2", address="address2", home="home2", mobile="mobile2",
+            work="work2", fax="fax2", email="email2", email2="email2_2", email3="email3_2",
+            homepage="homepage2")
+]
